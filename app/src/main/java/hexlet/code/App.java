64342e83aad36173d4bff036c6eadcd0109dcdc7
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2024-01-23 18:51:51 +0300

    Рефакторинг

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,16 +2,48 @@ package hexlet.code;
 
 import hexlet.code.games.EvenGame;
 
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
-        switch (Cli.gameMenu()) {
+        switch (gameMenu()) {
             case Greet -> Cli.askUsername();
             case Even -> EvenGame.game();
-            case Exit -> Cli.exitGame();
+            case Exit -> exitGame();
             case Unknown -> {
-                Cli.printUnknownGameMessage();
-                Cli.exitGame();
+                printUnknownGameMessage();
+                exitGame();
             }
         }
+    }
+
+    public static Games gameMenu() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        String gameNumberString = scanner.nextLine();
+        int gameNumber;
+        try {
+            gameNumber = Integer.parseInt(gameNumberString);
+        } catch (Exception ex) {
+            gameNumber = -1;
+        }
+        return switch (gameNumber) {
+            case 1 -> Games.Greet;
+            case 2 -> Games.Even;
+            case 0 -> Games.Exit;
+            default -> Games.Unknown;
+        };
+    }
+
+    public static void exitGame() {
+        System.exit(0);
+    }
+
+    public static void printUnknownGameMessage() {
+        System.out.println("Unknown game");
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,27 +4,6 @@ import java.util.Scanner;
 
 public class Cli {
 
-    public static Games gameMenu() {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("0 - Exit");
-        Scanner scanner = new Scanner(System.in);
-        String gameNumberString = scanner.nextLine();
-        int gameNumber;
-        try {
-            gameNumber = Integer.parseInt(gameNumberString);
-        } catch (Exception ex) {
-            gameNumber = -1;
-        }
-        return switch (gameNumber) {
-            case 1 -> Games.Greet;
-            case 2 -> Games.Even;
-            case 0 -> Games.Exit;
-            default -> Games.Unknown;
-        };
-    }
-
     public static String askUsername() {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
@@ -37,13 +16,5 @@ public class Cli {
     public static String readLine() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
-    }
-
-    public static void exitGame() {
-        System.exit(0);
-    }
-
-    public static void printUnknownGameMessage() {
-        System.out.println("Unknown game");
     }
 }
