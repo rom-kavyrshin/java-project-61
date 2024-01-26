47d1d7837767be7b4f94faa6cd5f899abcfe4387
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2024-01-26 14:35:48 +0300

    Обновлен build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     application
     checkstyle
 }
@@ -9,15 +8,6 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 application {
