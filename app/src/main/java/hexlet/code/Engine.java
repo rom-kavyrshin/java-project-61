ec1811b4851a1856019ca3314cb9209603522644
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2024-01-26 17:01:54 +0300

    Удалены константы и format

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -6,14 +6,6 @@ public class Engine {
 
     public static final int COUNT_OF_ROUNDS = 3;
 
-    private static final String QUESTION_PART_STRING = "Question: ";
-    private static final String WAIT_ANSWER_STRING = "Your answer: ";
-
-    private static final String RIGHT_ANSWER_CONGRATULATION_STRING = "Correct!";
-    private static final String WRONG_ANSWER_EXPLAIN_PATTERN = "'%1$s' is wrong answer ;(. Correct answer was '%2$s'.\n"
-            + "Let's try again, %3$s!";
-    private static final String FINAL_CONGRATULATION_PATTERN = "Congratulations, %1$s!";
-
     private static String username;
 
     public static void welcomeGame(String rules) {
@@ -25,22 +17,26 @@ public class Engine {
         boolean wasMistake = false;
 
         for (Exercise exercise : exercises) {
-            System.out.println(QUESTION_PART_STRING + exercise.question());
+            System.out.println("Question: " + exercise.question());
 
-            System.out.print(WAIT_ANSWER_STRING);
+            System.out.print("Your answer: ");
             String userAnswer = Utils.readLine();
 
             if (exercise.answer().equals(userAnswer)) {
-                System.out.println(RIGHT_ANSWER_CONGRATULATION_STRING);
+                System.out.println("Correct!");
             } else {
                 wasMistake = true;
-                System.out.printf(WRONG_ANSWER_EXPLAIN_PATTERN + "%n", userAnswer, exercise.answer(), username);
+                System.out.println(
+                    "'" + userAnswer + "' is wrong answer ;(. "
+                    + "Correct answer was '" + exercise.answer() + "'.\n"
+                    + "Let's try again, " + username + "!"
+                );
                 break;
             }
         }
 
         if (!wasMistake) {
-            System.out.printf(FINAL_CONGRATULATION_PATTERN + "%n", username);
+            System.out.println("Congratulations, " + username + "!");
         }
     }
 
